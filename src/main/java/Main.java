a6d256121e303fbb0c5fcb3c215b643db9e2c4cb
Joshua <joshualarsen108@gmail.com>
2023-03-20 12:56:39 -0400

    Added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        //super class
         Animal a1 = new Animal();
         a1.sound();
+        //method overloading
         a1.sound("bark");
+        //subclass
         Cat cat = new Cat();
+        //method overriding
         cat.sound();
     }
 }
