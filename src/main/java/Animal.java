c9bb575f2f52a7d04f555325db3f6feda40d58b4
Joshua <joshualarsen108@gmail.com>
2023-03-20 12:53:47 -0400

    Basics

---
+++ src/main/java/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public void sound() {
+        System.out.println("noise");
+    }
+
+    public void sound(String string) {
+        System.out.println(string);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Animal();
+        a1.sound();
+        a1.sound("bark");
+        Cat cat = new Cat();
+        cat.sound();
+    }
+}
